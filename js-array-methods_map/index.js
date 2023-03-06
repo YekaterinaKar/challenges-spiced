72481fb60484222aee74f27290cfde3079def695
Katja <karnashauskaite@gmail.com~>
2023-03-06 16:34:04 +0100

    map

--- js-array-methods_map/index.js
+++ js-array-methods_map/index.js
@@ -24,10 +24,24 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+}); // ['as often as you like.', ...]
+console.log(lowerCaseAnswers);
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question + " " + card.answer;
+});
+console.log(questionsAndAnswersTogether);
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+// ["How often can I use <header>? - As often as you like.", ...]
+
+const questionAndAnswer = cards.map((card) => {
+  return `Question: ${card.question}  answer: ${card.answer}`;
+});
+
+console.log(questionAndAnswer);
+
+// [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
